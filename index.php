1e0d88d23727edaeec6a77132cd58706ac3fd72a
Marty <mthubmann@gmail.com>
2022-04-01 21:06:12 -0400

    Adding Required Content
    Adding some required content for the site to work.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$item1="http://localhost/5Things/";
+$item2="https://www.youtube.com/watch?v=RHzhw97F7n4";
+$item3="Order Number: W882903276";
+$item4="https://github.com/ezSQL/ezsql";
+$item5="http://192.168.0.253/HomeNAS";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+</head>
+<body>
+Here's the last 5 things for you to remember
+
+
+
+
+</body>
+</html>
