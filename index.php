93582bd1f03ae73a9fe98d1f241fa6c754686fb8
Marty <mthubmann@gmail.com>
2022-04-29 17:19:21 -0400

    Resolve bootstrap-popper issues
    Correct file issues leading to problems using the local files for bootstrap in lieu of a CDN.

--- index.php
+++ index.php
@@ -309,13 +309,15 @@ for($ii = 0;$ii<count($ShortTermMem);$ii++){
 		</div>
 	</div>
 </nav>
-<!--
+
 <script type="text/javascript" src="<?php echo $address;?>jquery.js"></script>
-<script type="text/javascript" src="<?php echo $address;?>bootstrap.bundle.min.js"></script>!-->
-<!-- !-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?php echo $address;?>popper.min.js"></script>
+<script type="text/javascript" src="<?php echo $address;?>bootstrap.min.js"></script>!-->
+<!--<!-- !-->
+<!--
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 <!-- !-->
 <script>
 
